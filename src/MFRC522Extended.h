c893849db5677c73a7cf27b04663453876f378dc
lockbox25 <yellowdino19@outlook.com>
2026-02-22 18:31:45 +0000

    Add SPIClass* to MFRC522Extended.h

--- src/MFRC522Extended.h
+++ src/MFRC522Extended.h
@@ -7,7 +7,7 @@
 #define MFRC522Extended_h
 
 #include <Arduino.h>
-#include "MFRC522.h"
+#include "MFRC522-mbed-giga.h"
 
 class MFRC522Extended : public MFRC522 {
 		
@@ -78,8 +78,11 @@ public:
 	// Contructors
 	/////////////////////////////////////////////////////////////////////////////////////
 	MFRC522Extended() : MFRC522() {};
+	MFRC522Extended(SPIClass* spi = &SPI) : MFRC522(spi) {};
 	MFRC522Extended(uint8_t rst) : MFRC522(rst) {};
+	MFRC522Extended(uint8_t rst, SPIClass* spi = &SPI) : MFRC522(rst, spi) {};
 	MFRC522Extended(uint8_t ss, uint8_t rst) : MFRC522(ss, rst) {};
+	MFRC522Extended(uint8_t ss, uint8_t rst, SPIClass* spi = &SPI) : MFRC522(ss, rst, spi) {};
 	
 	/////////////////////////////////////////////////////////////////////////////////////
 	// Functions for communicating with PICCs
